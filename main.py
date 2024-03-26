1695a6c685281ea8490c339c65daeffd8abbb34b
xRumpel <164161359+xRumpel@users.noreply.github.com>
2024-03-26 17:05:57 +0700

    const and new const

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import pygame
 pygame.init()
 
+#Создание окна
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("My Game is TIR")
+
+icon = pygame.image.load("icon.png")
+pygame.display.set_icon(icon)
 
 #Игровой цикл
 running = True
